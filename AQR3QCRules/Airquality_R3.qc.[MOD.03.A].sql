9d00a25fe092d14c60aed868512d3a08c9b33720
Ana Diez <adiez@bilbomatica.es>
2025-07-07 09:17:50 +0200

    287691 View MOD.03.A - Collate and new comments

--- AQR3QCRules/Airquality_R3.qc.[MOD.03.A].sql
+++ AQR3QCRules/Airquality_R3.qc.[MOD.03.A].sql
@@ -1,7 +1,7 @@
 USE [Airquality_R3]
 GO
 
-/****** Object:  View [qc].[MOD.03.A]    Script Date: 01/07/2025 ******/
+/****** Object:  View [qc].[MOD.03.A]    Script Date: 07/07/2025 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -15,6 +15,8 @@ GO
 CREATE VIEW [qc].[MOD.03.A] AS
 
 -- Creation date: June 2025
+-- QC rule code: MOD.03.A
+-- QC rule name: MOD.03.A Vocabulary - [DataAggregationProcessId]
 
 WITH CTE_model AS ( 
     SELECT 
@@ -28,7 +30,7 @@ missing_codes AS (
        *
     FROM CTE_model m 
     LEFT JOIN reference.Vocabulary v 
-        ON m.dataaggregationprocessid = v.notation 
+        ON m.dataaggregationprocessid = v.notation COLLATE Latin1_General_CI_AS
         AND v.vocabulary = 'aggregationprocess'
     WHERE v.notation IS NULL
 	      AND m.dataaggregationprocessid IS NOT NULL
