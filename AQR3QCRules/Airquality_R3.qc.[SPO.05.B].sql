f2a96d8e659f9af3ace1a7b1a3684ab971d9794d
goitisolo <58937813+goitisolo@users.noreply.github.com>
2025-09-02 14:19:51 +0200

    STA.PK ,STA.07.A, SPO.PK, SPO.05.A ,SPO.05.B
    STA.PK and STA.07.A,  SPO.PK, SPO.05.A and SPO.05.B
    https://taskman.eionet.europa.eu/issues/290547
    https://taskman.eionet.europa.eu/issues/290545
    https://taskman.eionet.europa.eu/issues/290544

---
+++ AQR3QCRules/Airquality_R3.qc.[SPO.05.B].sql
@@ -0,0 +1,37 @@
+USE [Airquality_R3]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [qc].[SPO.05.B] AS
+
+-- Creation date: 02 September 2025
+-- QC rule code: SPO.05.B
+-- QC rule name:  SPO.05.B Constraint - [AirQualityStationArea] SPO.02
+
+WITH CTE_samplingpoint AS (
+  SELECT    
+    CASE WHEN "AirQualityStationArea" = '' THEN NULL ELSE "AirQualityStationArea" END AS "AirQualityStationArea",
+    CASE WHEN "AssessmentMethodId" = '' THEN NULL ELSE "AssessmentMethodId" END AS "AssessmentMethodId"
+  FROM reporting."samplingpoint"
+  WHERE "AirQualityStationArea" IS NOT NULL AND "AssessmentMethodId" IS NOT NULL
+),
+
+duplicate_spo_pairs AS (
+  SELECT 
+   AirQualityStationArea,
+    AssessmentMethodId
+  FROM CTE_samplingpoint s
+  GROUP BY AirQualityStationArea, AssessmentMethodId
+  HAVING COUNT(*) > 1
+)
+
+SELECT 
+s.*
+FROM CTE_samplingpoint s
+INNER JOIN duplicate_spo_pairs d
+  ON s.AirQualityStationArea = d.AirQualityStationArea
+ AND s.AssessmentMethodId = d.AssessmentMethodId
---
+++ AQR3QCRules/Airquality_R3.qc.[STA.PK].sql
@@ -0,0 +1,43 @@
+USE [Airquality_R3]
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [qc].[STA.PK] AS
+
+-- Creation date: 02 September 2025
+-- QC rule code: STA.PK
+-- QC rule name: STA.PK Constraint - [CountryCode,AirQualityStationEoICode]
+
+WITH CTE_station AS (
+  SELECT 
+    
+    NULLIF("airqualitystationeoicode", '') AS airqualitystationeoicode,
+    NULLIF("CountryCode", '') AS CountryCode
+   
+  FROM reporting."station"
+),
+duplicate_sta_records AS (
+  SELECT 
+    airqualitystationeoicode,
+    CountryCode
+  FROM CTE_station 
+  GROUP BY airqualitystationeoicode, CountryCode
+  HAVING COUNT(*) > 1
+)
+SELECT 
+  /*a.record_id,*/a.airqualitystationeoicode,a.CountryCode
+FROM CTE_station a
+LEFT JOIN duplicate_sta_records d
+  ON a.airqualitystationeoicode = d.airqualitystationeoicode
+ AND a.CountryCode = d.CountryCode
+ 
+WHERE d.airqualitystationeoicode IS NOT NULL  -- duplicity
+   OR a.airqualitystationeoicode IS NULL 
+   OR a.CountryCode IS NULL
+   
+
+
