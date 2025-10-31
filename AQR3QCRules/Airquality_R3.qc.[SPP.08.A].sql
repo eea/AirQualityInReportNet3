bd3dc47af8e2f9aa3626a0f5010eb722a3ffb752
Ana Diez <adiez@bilbomatica.es>
2025-10-31 11:43:58 +0100

    287678 SPP_08_A & 287679 SPP_09_A output small improvements

--- AQR3QCRules/Airquality_R3.qc.[SPP.08.A].sql
+++ AQR3QCRules/Airquality_R3.qc.[SPP.08.A].sql
@@ -1,7 +1,7 @@
 USE [Airquality_R3]
 GO
 
-/****** Object:  View [qc].[SPP.08.A]    Script Date: 22/10/2025 ******/
+/****** Object:  View [qc].[SPP.08.A]    Script Date: 31/10/2025 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,18 +12,21 @@ GO
 CREATE VIEW [qc].[SPP.08.A] AS
 
 -- Creation date: 22 October 2025
--- QC rule code: SPP.08.A
--- QC rule name: SPP.08.A Vocabulary - [MeasurementMethod]
+-- QC rule code: SPP_08_A
+-- QC rule name: SPP_08_A Vocabulary - [MeasurementMethod]
+-- Modification date: 31 October 2025
 
 
 WITH CTE_samplingprocess AS ( 
 SELECT /*record_id ,*/
+  CASE WHEN "countrycode" = '' THEN NULL ELSE "countrycode" END as "countrycode",
+  CASE WHEN "assessmentmethodid" = '' THEN NULL ELSE "assessmentmethodid" END as "assessmentmethodid",
   CASE WHEN "measurementmethod" = '' THEN NULL ELSE "measurementmethod" END as "measurementmethod",
   CASE WHEN "measurementtype" = '' THEN NULL ELSE "measurementtype" END as "measurementtype"  
 FROM reporting."samplingprocess" ) 
 
 , missing_codes AS ( 
-SELECT /*sp.record_id,*/ sp."measurementtype", sp."measurementmethod"
+SELECT /*sp.record_id,*/ sp."countrycode", sp."assessmentmethodid", sp."measurementtype", sp."measurementmethod"
 FROM CTE_samplingprocess sp 
 LEFT JOIN reference."vocabulary" v 
 ON sp."measurementmethod" = v."notation" COLLATE Latin1_General_CI_AS AND v."vocabulary" = 'measurementmethod'
--- AQR3QCRules/Airquality_R3.qc.[SPP.09.A].sql
+++ AQR3QCRules/Airquality_R3.qc.[SPP.09.A].sql
@@ -1,7 +1,7 @@
 USE [Airquality_R3]
 GO
 
-/****** Object:  View [qc].[SPP.09.A]    Script Date: 23/10/2025 ******/
+/****** Object:  View [qc].[SPP.09.A]    Script Date: 31/10/2025 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,18 +12,21 @@ GO
 CREATE VIEW [qc].[SPP.09.A] AS
 
 -- Creation date: 23 October 2025
--- QC rule code: SPP.09.A
--- QC rule name: SPP.09.A Vocabulary - [MeasurementEquipment]
+-- QC rule code: SPP_09_A
+-- QC rule name: SPP_09_A Vocabulary - [MeasurementEquipment]
+-- Modification date: 31 October 2025
 
 
 WITH CTE_samplingprocess AS ( 
 SELECT /*record_id ,*/
+  CASE WHEN "countrycode" = '' THEN NULL ELSE "countrycode" END as "countrycode",
+  CASE WHEN "assessmentmethodid" = '' THEN NULL ELSE "assessmentmethodid" END as "assessmentmethodid",
   CASE WHEN "measurementtype" = '' THEN NULL ELSE "measurementtype" END as "measurementtype",
   CASE WHEN "measurementequipment" = '' THEN NULL ELSE "measurementequipment" END as "measurementequipment" 
 FROM reporting."samplingprocess" ) 
 
 , missing_codes AS ( 
-SELECT /*sp.record_id,*/ sp."measurementtype", sp."measurementequipment" 
+SELECT /*sp.record_id,*/ sp."countrycode", sp."assessmentmethodid", sp."measurementtype", sp."measurementequipment" 
 FROM CTE_samplingprocess sp 
 LEFT JOIN reference."vocabulary" v 
 ON sp."measurementequipment" = v."notation" COLLATE Latin1_General_CI_AS AND v."vocabulary" = 'measurementequipment'
@@ -39,4 +42,5 @@ SELECT DISTINCT * FROM missing_codes
 
 
 GO
+
 
