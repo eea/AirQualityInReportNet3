4cd117dd402e6ae4470e76853c8ad2efbb3d2f78
Ana Diez <adiez@bilbomatica.es>
2025-11-20 17:05:14 +0100

    290545 SPO_05_B only 1 AirQualityStationArea per AssessmentMethodId
    There can be only one value under AirQualityStationArea for each reported AssessmentMethodId.

--- AQR3QCRules/Airquality_R3.qc.[SPO_05_B].sql
+++ AQR3QCRules/Airquality_R3.qc.[SPO_05_B].sql
@@ -1,37 +1,50 @@
 USE [Airquality_R3]
 GO
 
+/****** Object:  View [qc].[SPO_05_B]    Script Date: 20/11/2025 16:57:28 ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
+
 
 CREATE VIEW [qc].[SPO_05_B] AS
 
 -- Creation date: 02 September 2025
 -- QC rule code: SPO_05_B
 -- QC rule name:  SPO_05_B Constraint - [AirQualityStationArea] SPO_02
+-- Modification date: 20 November 2025
 
-WITH CTE_samplingpoint AS (
-  SELECT    
-    CASE WHEN "AirQualityStationArea" = '' THEN NULL ELSE "AirQualityStationArea" END AS "AirQualityStationArea",
-    CASE WHEN "AssessmentMethodId" = '' THEN NULL ELSE "AssessmentMethodId" END AS "AssessmentMethodId"
-  FROM reporting."samplingpoint"
-  WHERE "AirQualityStationArea" IS NOT NULL AND "AssessmentMethodId" IS NOT NULL
-),
-
-duplicate_spo_pairs AS (
+WITH 
+CTE_samplingPoint AS (
   SELECT 
-   AirQualityStationArea,
-    AssessmentMethodId
-  FROM CTE_samplingpoint s
-  GROUP BY AirQualityStationArea, AssessmentMethodId
-  HAVING COUNT(*) > 1
+    --record_id
+    CASE WHEN "AirQualityStationArea" = '' THEN NULL 
+      ELSE "AirQualityStationArea" END as "AirQualityStationArea"
+    ,CASE WHEN "AssessmentMethodId" = '' THEN NULL 
+      ELSE "AssessmentMethodId" END AS "AssessmentMethodId"
+    FROM reporting."samplingpoint"
 )
 
-SELECT 
-s.*
-FROM CTE_samplingpoint s
-INNER JOIN duplicate_spo_pairs d
-  ON s.AirQualityStationArea = d.AirQualityStationArea
- AND s.AssessmentMethodId = d.AssessmentMethodId
+, several_AirQualityStationArea AS ( 
+  SELECT DISTINCT 
+         sp."AssessmentMethodId",
+         --COUNT(DISTINCT(sp."AirQualityStationArea")) -- Reportnet 3
+         COUNT(DISTINCT(sp."AirQualityStationArea")) AS CountingAirQualityStationArea -- SQL Server view
+  FROM CTE_samplingPoint sp 
+  GROUP BY sp."AssessmentMethodId"
+  HAVING COUNT(DISTINCT(sp."AirQualityStationArea")) > 1
+) 
+
+SELECT --record_id -- Reportnet 3
+       DISTINCT sp."AssessmentMethodId" -- SQL Server view
+FROM CTE_samplingPoint sp 
+INNER JOIN several_AirQualityStationArea saqsa
+ON sp."AssessmentMethodId" = saqsa."AssessmentMethodId"
+
+
+GO
+
+
