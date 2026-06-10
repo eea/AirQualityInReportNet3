e0aa50f06319a7f277cdfcc1cf6da0ab86f0fd4d
goitisolo <tgoitisolo@bilbomatica.es>
2026-06-10 12:05:26 +0200

    QC rules for SamplingPoint (SPO) table
    optimization and testing of SPO views
    https://taskman.eionet.europa.eu/issues/303815

--- AQR3QCRules/Airquality_R3.qc.[SPO_03_A].sql
+++ AQR3QCRules/Airquality_R3.qc.[SPO_03_A].sql
@@ -1,7 +1,7 @@
 USE [Airquality_R3]
 GO
 
-/****** Object:  View [qc].[SPO.03.A]    Script Date: 25/05/2026 14:32:52 ******/
+/****** Object:  View [qc].[SPO_03_A]    Script Date: 10/06/2026 11:57:38 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -12,21 +12,30 @@ GO
 
 CREATE VIEW [qc].[SPO_03_A] AS
 
--- Creation date: July 2025
+-- Creation date: June 2026
 -- QC rule code: SPO.03.A
 -- QC rule name: SPO.03.A Vocabulary - [AirPollutantCode]
 
 WITH CTE_samplingpoint AS ( 
 SELECT --record_id ,
-CASE WHEN "PollutantId" = '' THEN NULL ELSE "PollutantId" END as "PollutantId" 
-FROM reporting."samplingpoint" ) 
+NULLIF(LTRIM(RTRIM([PollutantId])), '') AS [PollutantId]
+FROM reporting."samplingpoint" ) ,
 
-,missing_codes AS ( 
+vocab AS (
+SELECT
+v.[ID],
+RIGHT(v.[ID], CHARINDEX('/', REVERSE(v.[ID])) - 1) AS [PollutantSuffix]
+FROM [reference].[vocabulary] v
+WHERE v.[vocabulary] = 'pollutant'
+),
+
+
+missing_codes AS ( 
 SELECT /*sp.record_id,*/ sp."PollutantId" 
 FROM CTE_samplingpoint sp 
-LEFT JOIN reference."vocabulary" v 
-ON sp."PollutantId" = v."notation" AND v."vocabulary" = 'pollutant'
-WHERE v."notation" IS NULL ) 
+LEFT JOIN vocab v
+ON v.[PollutantSuffix] = sp.[PollutantId]
+WHERE v.[ID] IS NULL)
 
 SELECT DISTINCT * FROM missing_codes
 
